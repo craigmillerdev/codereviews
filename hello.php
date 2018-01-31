78672b6f64a0b7801445aab1530629b88fe35419
Craig Miller <hello@craigmiller.co.uk>
2018-01-31 21:32:19 +0000

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,7 +1,17 @@
 <?php
 
-$string = "Hello %s\n";
+$hour = date('G');
 
-echo sprintf($string, $argv[1]);
+if ( $hour >= 5 && $hour <= 11 ) {
+    $greeting = "Good Morning";
+} else if ( $hour >= 12 && $hour <= 18 ) {
+    $greeting = "Good Afternoon";
+} else if ( $hour >= 19 || $hour <= 4 ) {
+    $greeting = "Good Evening";
+}
+
+$string = "$s %s\n";
+
+echo sprintf($string, $greeting, $argv[1]);
 
 exit(0);
